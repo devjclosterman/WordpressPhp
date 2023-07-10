5aa43f114c82a928854f3299b4f1881cc73dcba1
Jason Closterman <devjclosterman@gmail.com>
2023-07-10 09:11:49 -0700

    PhpArrays

--- practice.php
+++ practice.php
@@ -47,4 +47,31 @@ $jason = array(
             'age' => 33,
             'job' => 'Gallery Curator',
         ),  
-    )
+    );
+
+    print_r( $brothers );
+    echo $brothers['rob']['job'];
+
+$a = 2;    
+echo $a + 2; //output 4
+
+//Modules operator
+$a = 2;
+$b = 3;
+echo $b % $a; //Will print out 1;
+
+//Dont forget PEMDAS 
+
+echo 3**2;//9
+echo 5**2 * ( 6 + 3 ) - 1; //224
+
+<?php
+   $results = array();
+   $results[] = 9 + ( 2 * 5 ) - 11; //8
+   $results[] = 18 / ( ( 3 * 6 ) - 9 ) * 3; //6
+   $results[] = 3 * ( ( 24 / 2) - ( 3 * 4 ) * ( 2 * 6 ) ); //36
+
+   echo $results[0] . "\n";
+   echo $results[1] . "\n";
+   echo $results[2] . "\n";
+   print_r( $results )
